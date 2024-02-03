82d6b4d1fb4cb45d1e8a983b113a5a3cbc1d9cb5
Ksll <anzilparviz03@gmail.com>
2024-02-03 16:43:21 +0530

    added mapactivity

---
+++ Android App/app/src/main/java/com/example/systemx64/MapActivity.java
@@ -0,0 +1,38 @@
+package com.example.systemx64;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap googleMap;
+    double lat1,lon1;
+    String distrinctname="DK";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+        Map<String, Object> hotspot=new HashMap<>();
+
+
+
+
+
+    }
+}
