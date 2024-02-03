fad63dedaec457593dcca1124e473f7d44fad651
Ksll <anzilparviz03@gmail.com>
2024-02-04 01:07:14 +0530

    added onclickmarker

--- Android App/app/src/main/java/com/example/systemx64/MapActivity.java
+++ Android App/app/src/main/java/com/example/systemx64/MapActivity.java
@@ -1,9 +1,10 @@
 package com.example.systemx64;
 
-import android.content.Intent;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -11,6 +12,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
@@ -19,37 +21,70 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private GoogleMap googleMap;
 
     public ArrayList<LatLng> locationList;
+    public ArrayList<String> districtList;
+    public ArrayList<String> stateList;
+    public ArrayList<Integer> rapeCountList;
+    public int year;
+    public String crimetype;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-        Intent intent=getIntent();
-         locationList = (ArrayList<LatLng>) intent.getSerializableExtra("locationlist");
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            locationList = (ArrayList<LatLng>) extras.getSerializable("locationlist");
+            districtList = extras.getStringArrayList("districtlist");
+            crimetype=getIntent().getStringExtra("Crimetype");
+            stateList = extras.getStringArrayList("statelist");
+            year = extras.getInt("Year");
 
-
+            rapeCountList = extras.getIntegerArrayList("rapecountlist");
+        }
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
     }
 
     @Override
-
     public void onMapReady(@NonNull GoogleMap googleMap) {
         this.googleMap = googleMap;
 
-
         if (locationList != null) {
-
-            for (LatLng latLng : locationList) {
-                googleMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+            for (int i = 0; i < locationList.size(); i++) {
+                LatLng latLng = locationList.get(i);
+                MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("Marker " + (i + 1));
+                googleMap.addMarker(markerOptions);
             }
-
-
             LatLng firstLocation = locationList.get(0);
             googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(firstLocation, 12));
         }
+
+        googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                int position = Integer.parseInt(marker.getTitle().split(" ")[1]) - 1;
+                showMarkerDetailsDialog(position);
+                return true;
+            }
+        });
     }
 
+    private void showMarkerDetailsDialog(int position) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Marker Details")
+                .setMessage(
+                        "District: " + districtList.get(position) + "\n" +
+                        crimetype+" Count: " + rapeCountList.get(position)+"\n"+
+                        "Year: "+year)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
 }
